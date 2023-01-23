a826681dcbf2c5b80f897f0c916d3411ad5cdba5
sureshp-snow <123354180+sureshp-snow@users.noreply.github.com>
2023-01-23 12:32:02 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,4 +3,6 @@ import streamlit
 streamlit.title('My Parents Healthy Diner')
 
 streamlit.header('Breakfast Menu')
-streamlit.text('Omega3 & Blueberry oat meal')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
