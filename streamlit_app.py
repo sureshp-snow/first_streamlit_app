138b8401798b0e31767c9f09c35b8ca1abe2a3d9
sureshp-snow <123354180+sureshp-snow@users.noreply.github.com>
2023-01-30 13:32:09 +0530

    Update streamlit_app.py
    added text box for fruit name input

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,9 +24,11 @@ streamlit.dataframe(fruits_to_show)
 
 # New section to display Fruityvice results
 streamlit.header("Fruityvice Fruit Advice!")
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
 
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 #streamlit.text(fruityvice_response.json())
 
 
