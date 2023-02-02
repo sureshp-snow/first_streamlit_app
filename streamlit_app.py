9c7ca12cc9b2f98178eae74647d89bfce55db184
sureshp-snow <123354180+sureshp-snow@users.noreply.github.com>
2023-02-02 11:04:59 +0530

    Update streamlit_app.py
    added snowflake

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,13 @@ import streamlit
 import pandas
 import snowflake.connector
 
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
+
 streamlit.title('My Parents Healthy Diner')
 
 streamlit.header('Breakfast Favorites')
