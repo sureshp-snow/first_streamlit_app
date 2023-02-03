6ea0f099462d077c5dcdf5be8f173e7676061c3c
sureshp-snow <123354180+sureshp-snow@users.noreply.github.com>
2023-02-03 10:13:33 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -83,7 +83,7 @@ def inser_row_snowflake(new_fruit):
         return "Thanks for adding " + new_fruit
     
 add_my_fruit = streamlit.text_input('What fruit would you like to add?')
-if streamlit.button('Add a fruit to list')
+if streamlit.button('Add a fruit to list'):
     my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
     back_from_function = inser_row_snowflake(add_my_fruit)
     streamlit.text(back_from_function)
